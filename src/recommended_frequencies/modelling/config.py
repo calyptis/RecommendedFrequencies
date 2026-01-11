89e14064454a924583f0d132a952714320339f28
chizzali <chizzali@kth.se>
2026-01-11 16:25:05 +0100

    Move variables to config

--- src/recommended_frequencies/modelling/config.py
+++ src/recommended_frequencies/modelling/config.py
@@ -1,3 +1,5 @@
+"""Variables for ML training and inference."""
+
 import os
 
 from recommended_frequencies.config import MODEL_DIR, PREPARED_DATA_DIR
@@ -26,3 +28,7 @@ CATBOOST_FEATURES = EUCLIDEAN_FEAT_COLS
 ML_DATA_FILE = os.path.join(PREPARED_DATA_DIR, "ml_data.csv")
 
 CATBOOST_MODEL_FILE = os.path.join(MODEL_DIR, "Catboost.cbm")
+
+# Sampling data
+ADDTL_POS_FRAC = 0.4
+TRAIN_FRAC = 0.8
